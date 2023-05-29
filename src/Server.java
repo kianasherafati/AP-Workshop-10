f7d359fc076ad977f156dd2d4611ae735d2c10b8
kiana <kiana.sherafati@gmail.com>
2023-05-30 01:35:15 +0330

    Edited Server class

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server implements Runnable{
 
     @Override
     public void run() {
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            Socket client = server.accept();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
